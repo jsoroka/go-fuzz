03acc4fb7e7dd7abbf81399f38f2fe6c2539ed4d
Joe Soroka <gits@joesoroka.com>
2022-04-02 20:01:37 -0700

    allow using this fork, until changes are upstreamed

--- go-fuzz/cover.go
+++ go-fuzz/cover.go
@@ -8,8 +8,8 @@ import (
 	"log"
 	"os"
 
-	. "github.com/dvyukov/go-fuzz/go-fuzz-defs"
-	. "github.com/dvyukov/go-fuzz/internal/go-fuzz-types"
+	. "github.com/jsoroka/go-fuzz/go-fuzz-defs"
+	. "github.com/jsoroka/go-fuzz/internal/go-fuzz-types"
 )
 
 func makeCopy(data []byte) []byte {
